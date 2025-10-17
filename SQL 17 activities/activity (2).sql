578a9e8280732d08001894eb7054e4c4f849fe80
Paulo E. Heymann <pehlpgamer@gmail.com>
2025-10-17 18:13:38 +0800

    Add files via upload

--- SQL 17 activities/activity (2).sql
+++ SQL 17 activities/activity (2).sql
@@ -1,15 +1,23 @@
-# Activity 2
-drop database if exists ;
-create database if not exists ;
-use ;
+use addressbook;
 
-create table table1(
-varchar(255),
-varchar(255)
-);
+
 
 insert into () values(
 
 );
 
+# 1.
 select * from ;
+
+# 2.
+select * from ;
+
+# 3. It will display all columns that
+select * from employee_data where salary <= 100000;
+
+
+# 4.
+select salary, perks from employee_data where perks > 16000;
+
+# 5.
+select l_name, f_name from employee_data where title = 'Marketing Excutive';
